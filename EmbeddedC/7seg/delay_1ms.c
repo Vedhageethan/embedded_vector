6b4e53626e8c99860365de6011f6948aba2235e1
vedhageethan <vedhaggkbt99@gmail.com>
2026-01-15 16:44:57 +0530

    nn

--- EmbeddedC/7seg/delay_1ms.c
+++
@@ -1,9 +0,0 @@
-void delay_1ms(int dly)
-{
-	unsigned char d;
-		for(;dly>0;dly--)
-		{
-			for(d=250;d>0;d--);
-			for(d=247;d>0;d--);
-		}
-}
--- data_acq/delay_1ms.c
+++
@@ -1,9 +0,0 @@
-void delay_1ms(int dly)//to create 1ms delay this runs based on machine cycle.
-{
-	unsigned char d;
-		for(;dly>0;dly--)//used to increase the machine cycles of compilation.
-		{
-			for(d=250;d>0;d--);
-			for(d=247;d>0;d--);
-		}
-}
